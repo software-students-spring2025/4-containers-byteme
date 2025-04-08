ecb13d378c37be80938bc0ccd93f1be5a5009372
linahsan <lina.sanchez.nyc@gmail.com>
2025-04-08 16:04:05 -0400

    connecting to db and routes

--- web-app/app.py
+++ web-app/app.py
@@ -35,7 +35,6 @@ login_manager.init_app(app)
 # user class for login
 class User(UserMixin):
     """User class"""
-
     def __init__(self, user_data):
         self.id = str(user_data["_id"])
         self.username = user_data["username"]
