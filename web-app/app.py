4daa4610e07dae6a181b273813f35ff36af2de3d
linahsan <lina.sanchez.nyc@gmail.com>
2025-04-08 21:01:01 -0400

    updating app.py for html pages -- tbc

--- web-app/app.py
+++ web-app/app.py
@@ -6,7 +6,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 import pymongo
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+)
 from bson.objectid import ObjectId
 
 # loading env file
@@ -14,6 +21,7 @@ load_dotenv()
 
 # app setup
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 # mongodb setup
 mongo_host = os.getenv("MONGO_HOST")
@@ -30,6 +38,7 @@ bcrypt = Bcrypt(app)
 # login setup
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login_signup"
 
 
 # user class for login
@@ -41,6 +50,36 @@ class User(UserMixin):
         self.username = user_data["username"]
 
 
+# user login and registration
+@app.route("/login-signup", methods=["GET", "POST"])
+def login_signup():
+    """Render login/signup page"""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        action = request.form["submit"]  # this will be "Login" or "Sign Up"
+
+        if action == "Login":
+            user_data = users.find_one({"username": username})
+            if user_data and bcrypt.check_password_hash(
+                user_data["password"], password
+            ):
+                user = User(user_data)
+                login_user(user)
+                return redirect(url_for("home"))
+            return "Invalid credentials", 400
+
+        if action == "Sign Up":
+            if users.find_one({"username": username}):
+                return "User already exists", 400
+            hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+            user_data = {"username": username, "password": hashed_password}
+            user_id = users.insert_one(user_data).inserted_id
+            return redirect(url_for("login_signup"))
+
+    return render_template("login_signup.html")
+
+
 @login_manager.user_loader
 def load_user(user_id):
     """User loader"""
@@ -48,34 +87,27 @@ def load_user(user_id):
     return User(user_data) if user_data else None
 
 
-# user registration
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Render register page"""
-    if request.method == "POST":
-        return redirect(url_for("login"))
-    return render_template("register.html")
-
-
-# user login
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Render login page"""
-    if request.method == "POST":
-        return redirect(url_for("home"))
-    return render_template("login.html")
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    """Logout"""
+    logout_user()
+    return redirect(url_for("login_signup"))
 
 
 @app.route("/")
 def home():
     """Render home page"""
-    return render_template("page.html")
+    if not current_user.is_authenticated:
+        return redirect(url_for("login_signup"))
+    return render_template("index.html")
 
 
-@app.route("/journal")
-def journal():
+@app.route("/add-entry")
+@login_required
+def add_entry():
     """Render journaling page"""
-    return render_template("newEntry.html")
+    return render_template("new_entry.html")
 
 
 if __name__ == "__main__":
