0a38dea358de77c10f9c0aef26eec74a76a073d9
jessicahc <jhc10024@nyu.edu>
2025-04-10 13:44:05 -0400

    fix test_app linting

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -21,8 +21,9 @@ class MockUser:
         return self.id
 
 
+# pylint: disable=redefined-outer-name
 @pytest.fixture
-def client(): # pylint: disable=redefined-outer-name
+def client(): 
     """Fixture to create a test client for the Flask app."""
     app.config["TESTING"] = True
     with app.test_client() as client:
